ae54cb83aeaa199693da3fc80d1747331c4745b1
KADIRI SAI KUMAR <saikumarkadirik@gmail.com>
2026-07-15 18:09:53 +0530

    Add LeetCode 167: Two Sum II and update README

--- 12-Two-Pointers/0167-TwoSumII.java
+++ 12-Two-Pointers/0167-TwoSumII.java
@@ -1,10 +1,7 @@
 class TwoSumII {
-
     public static int[] twoSum(int[] numbers, int target) {
-
         int left = 0;
         int right = numbers.length - 1;
-
         while (left < right) {
 
             int sum = numbers[left] + numbers[right];
